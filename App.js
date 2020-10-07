16dc528cb39f5d907ccb6b78b6fcac74b8dab9ba
Maya <melokitty.1003@gmail.com>
2020-10-07 15:45:14 +0900

    API追加

--- App.js
+++ App.js
@@ -3,15 +3,26 @@ import React, { useState, useEffect } from 'react';
 import { StyleSheet, View, FlatList, SafeAreaView } from 'react-native';
 import ListItem from './components/ListItem';
 import dummyArticles from './dummies/articles.json';
+import Constants from 'expo-constants';
+import axios from 'axios';
+
+const URL = `http://newsapi.org/v2/top-headlines?country=jp&apiKey=${Constants.manifest.extra.newsApiKey}`;
 
 export default function App() {
   const [articles, setArticles] = useState([]);
   useEffect(() => {
-    const timer = setTimeout(() => {
-      setArticles(dummyArticles);
-    }, 2000);
-    return () => clearTimeout(timer);
+    fetchArticles();
   }, []);
+
+  const fetchArticles = async () => {
+    try {
+      const respones = await axios.get(URL);
+      setArticles(respones.data.articles);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
   return (
     <SafeAreaView style={styles.container}>
       <FlatList
