c98d3b406348659ac76548f88531c10df914dec3
Maya <melokitty.1003@gmail.com>
2020-10-13 22:04:02 +0900

    room永続化

--- store/index.js
+++ store/index.js
@@ -1,11 +1,24 @@
 import { createStore, combineReducers } from 'redux';
 import reducer from './reducers/user';
 import { composeWithDevTools } from 'redux-devtools-extension';
+import { persistReducer, persistStore } from 'redux-persist';
+import { AsyncStorage } from 'react-native';
 
 const rootReducer = combineReducers({
   user: reducer,
 });
 
-const store = createStore(rootReducer, composeWithDevTools());
+//rootを永続化
+const persistConfig = {
+  key: 'root',
+  storage: AsyncStorage,
+  whitelist: ['user'],
+};
+
+const persistedReducer = persistReducer(persistConfig, rootReducer);
+
+const store = createStore(persistedReducer, composeWithDevTools());
+
+export const persistor = persistStore(store);
 
 export default store;
